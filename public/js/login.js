74a04d79f89e97424c3781dac6fb88d9e7c55f4b
Saurav Kumar <agarwalsaurav6276@gmail.com>
2021-07-05 05:58:29 +0530

    Changes done

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ export const login=async(email,password)=>{
          color='#a64452';
         else
         color='#4bb543';
-        const markup=`<div class="alert alert--${type}" style="display:block;margin:auto;text-align:center;height:18%;background-color:${color};">${msg}</div>`;
+        const markup=`<div class="alert alert--${type}" style="display:block;margin:auto;text-align:center;height:22%;font-size:18px;background-color:${color};">${msg}</div>`;
         document.querySelector('body').insertAdjacentHTML('afterbegin',markup);
         window.setTimeout(hideAlert,5000);
     }
